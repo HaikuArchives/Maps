acd2727cb927684c89804da368f9f68e8b6eee21
PulkoMandy <pulkomandy@pulkomandy.tk>
2025-01-04 10:52:37 +0100

    Fix crash on exit

--- src/Maps.cpp
+++ src/Maps.cpp
@@ -5,13 +5,12 @@
 
 #include "Maps.h"
 
-Maps::Maps() : BApplication("application/x-vnd.Haiku-Maps") {		
+Maps::Maps() : BApplication("application/x-vnd.Haiku-Maps") {
 	mapsWindow = new MapsWindow();
 	mapsWindow->Show();
 }
 
 Maps::~Maps() {
-	delete mapsWindow;
 }
 
 int main(int argc, char *argv[]) {
